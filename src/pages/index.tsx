1386e79ee5a097f70ceec86d972d40a10e217a18
Aakash <aakash.thakur@meesho.com>
2023-02-01 14:42:36 +0530

    removed unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,7 @@
 // @ts-nocheck
-import { useRouter } from "next/router";
-import Script from "next/script";
-import React, { useEffect } from "react";
+import React from "react";
 
 const HomeAdaptive = (): React.ReactElement => {
-  const router: NextRouter = useRouter();
-  const { query } = router;
-
-  console.log(query);
-
-  useEffect(() => {}, [query]);
-
   const sendEvent = () => {
     window?.gtag("event", "payload?.action_name", {
       event_category: "123",
